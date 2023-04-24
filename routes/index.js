737cd5bb1b9602a7f6812e1f3c12ec3fa179c5de
Aquib <85285702+AquibPro@users.noreply.github.com>
2023-04-24 20:46:45 +0530

    First commit
    Nothing

---
+++ routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+const app = express();
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/submit', function(req, res, next) {
+  res.render('submit');
+
+  if(!req.body.username == "" || undefined || null && !req.body.message == "" || undefined || null) {
+
+let mailTransporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'alpha7x24@gmail.com',
+		pass: 'nrlxdqlnzrtixmrb'
+	}
+});
+
+let mailDetails = {
+	from: 'alpha7x24@gmail.com',
+	to: 'masteraquibkhan@gmail.com',
+	subject: `${req.body.username}, wants to contact you !`,
+	html: `<h3>His message - </h3><b><pre>${req.body.message}</pre></b>`
+};
+
+mailTransporter.sendMail(mailDetails, function(err, data) {
+	if(err) {
+		console.log('Error Occurred !');
+	} else {
+		console.log('Submitted successfully !');
+	}
+});
+  }else{res.render('/')}
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// handle POST request to /submit
+app.post('/submit', (req, res) => {
+  const username = req.body.username;
+  const message = req.body.message;
+  res.render('submit');
+});
+
+module.exports = router;
